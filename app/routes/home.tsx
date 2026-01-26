71f1557abdd24f9fb3fe8557893af967c507764a
Prerna Sharma <sharmaprerna393@gmail.com>
2026-01-27 00:15:43 +0530

    Complete initial UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import Resume from "~/components/Resume";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeIQ" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review Your Submissions and check AI-powered feedback.</h2>
+      </div>
+
+      {resumes.length >0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <Resume key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+    
+  </main>;
 }

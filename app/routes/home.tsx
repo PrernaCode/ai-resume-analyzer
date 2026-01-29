7bd07173df12215fe3bb49919aa4dc4dd73da99c
Prerna Sharma <sharmaprerna393@gmail.com>
2026-01-30 00:55:49 +0530

    feat: add authentication flow

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/Puter";
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
@@ -11,6 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {isLoading, auth} =usePuterStore();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const next = location.search.split('next=')[1];
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  },[auth.isAuthenticated]);
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
